16b258a91966ca2b57e701f9f3ae00595ee98cfe
Nel Benlloch <nel.benllochg@gmail.com>
2025-04-04 13:45:36 +0100

    ADD:
    Created the function which will calculate the Big N and will add the labels - Note that there is a placeholder of warnings to be added in this function

--- functions/Big_N.R
+++ functions/Big_N.R
@@ -13,37 +13,57 @@
 # Purpose
 # requirements
 # input
-
+# Author + Date
 
 ###################################################################
 ###################################################################
 
 # Error required for this code: distinct usubjid
-#test if number of rows equals to number of treatments
-# warning if some treatments have 0 as the summary
+# Error if some of the variables are missing in the dataset 
+#    - 'Empty' is ok, but they  cannot be found is Error
+# Test if number of rows equals to number of treatments
+# Warning if some treatments have 0 as the summary
 
 
 ###################################################################
 ###################################################################
 
 
-
+# Please add the Libraries used and the code description
+# Example input - output
 
 Big_N_calcs<-
     function(
      ds_in #Dataset in
     ,iid            = quo(usubjid)
-    ,by_group       = quo(trtan) #variable name - use the NUMERIC version
-    ,by_group_char  = quo(trta)
+    ,by_group       = trtan #variable name - use the NUMERIC version
+    ,by_group_char  = trta
     ,ds_out #Dataset out
     ){
-      
+      # add quotations to use in dplyr:
+      # Dtaset summary
       ds_out<-ds_in %>%
-            count(!!by_group) 
+            count( {{by_group}})
+      
+      unique_chars<-
+          ds_in %>%
+          select({{by_group}}, {{by_group_char}}) %>%
+          distinct()
+            
+      ds_out_char<-
+        ds_out%>%
+        left_join(unique_chars
+                 ,by = join_by({{by_group}}== {{by_group}}))%>%
+        select({{by_group}},{{by_group_char}},n) #neat order
 
-    return(ds_out)       
-      #test if number rows not equal to number distinct treatments
-      #warning - some of the treatments (is 0)
-
+    return(ds_out_char)       
     }
 
+
+####
+#  This area to test function:
+
+out<-Big_N_calcs(ds_in = ex_baseline2, ds_out=output)
+
+
+out 
