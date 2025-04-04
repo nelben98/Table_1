b94247df74bb85094f7d284bd348a749e0d67600
Nel Benlloch <nel.benllochg@gmail.com>
2025-04-04 14:00:08 +0100

    Addition of the Big N calculation to work out the percentage - and also start displaying the variables summaries

--- functions/Cts_func.R
+++ functions/Cts_func.R
@@ -13,11 +13,13 @@
 # Purpose
 # requirements
 # input
-
+# Author + Date
 
 ###################################################################
 ###################################################################
 
+
+# ERRORS TO BE ADDED:
 
 
 
@@ -28,12 +30,24 @@ library(tidyverse) #first load tidyverse then filter through:
 
 continuous_block<- function(
                        ds_in #Dataset in
-                      ,by_group = c('trtn') #variable name - use the NUMERIC version
+                      ,by_group       = trtan #variable name - use the NUMERIC version
+                      ,by_group_char  = trta
                       ,dps = 1
                       ,statistics = c('n','Mean','SD','Median','IQR','MinMax')
                       ,ds_out #Dataset out
                       ){
   
+  Big_N_calcs(ds_in
+             ,by_group
+             ,by_group_char)
+  
+  ds_in |> 
+    group_by({{by_group}}) |>
+    summarise(
+      mean = mean({{ expr }}),
+      sum = sum({{ expr }}),
+      n = n()
+    )
           
   
   
